800704e794f57d0acabf75ad875e8f395cfe4d5c
user <mohammed312k@gmail.com>
2024-07-27 16:53:07 +0530

    bugs are fixed

--- src/main/java/come/example1/example1/A.java
+++ src/main/java/come/example1/example1/A.java
@@ -1,4 +1,7 @@
 package come.example1.example1;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+    }
 }
---
+++ src/main/java/come/example1/example1/B.java
@@ -0,0 +1,7 @@
+package come.example1.example1;
+
+public class B {
+    public  void add(){
+        System.out.println("B add");
+    }
+}
